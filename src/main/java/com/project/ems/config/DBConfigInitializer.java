1e237618a11dd9aa5d6d71b028342809b397b863
sahil.pimenta <sahil.pimenta@gmail.com>
2025-06-15 19:17:32 +0530

    Added docker compose file for CI - V5

--- src/main/java/com/project/ems/config/DBConfigInitializer.java
+++ src/main/java/com/project/ems/config/DBConfigInitializer.java
@@ -1,4 +1,5 @@
 package com.project.ems.config;
+
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.context.EnvironmentAware;
@@ -6,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.ConfigurableEnvironment;
 import org.springframework.core.env.Environment;
 import org.springframework.core.env.MapPropertySource;
+import org.springframework.core.env.Profiles;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 import java.util.HashMap;
@@ -15,35 +17,40 @@ import java.util.Map;
 @Configuration
 public class DBConfigInitializer implements BeanPostProcessor, InitializingBean, EnvironmentAware {
 
-	  private JdbcTemplate jdbcTemplate;
-	  private ConfigurableEnvironment environment;
+	private JdbcTemplate jdbcTemplate;
+	private ConfigurableEnvironment environment;
 
-	  private final String propertySourceName = "dbProp";
+	private final String propertySourceName = "dbProp";
 
-	  public DBConfigInitializer(JdbcTemplate jdbcTemplate) {
-	    this.jdbcTemplate = jdbcTemplate;
-	  }
-
-	  @Override
-	  public void afterPropertiesSet() throws Exception {
-	    if (null != environment) {
-	      Map<String, Object> systemConfigMap = new HashMap<>();
-	      String sql = "SELECT param, value from config_values";
-	      List<Map<String, Object>> maps = jdbcTemplate.queryForList(sql);
-	      for (Map<String, Object> map : maps) {
-	        String param = String.valueOf(map.get("param"));
-	        Object value = map.get("value");
-	        systemConfigMap.put(param, value);
-	        System.out.println(String.format("param=%s, value=%s ", param, value));
-	      }
-	      environment.getPropertySources().addFirst(new MapPropertySource(propertySourceName, systemConfigMap));
-	    }
-	  }
-
-	  @Override
-	  public void setEnvironment(Environment environment) {
-	    if(environment instanceof ConfigurableEnvironment) {
-	      this.environment = (ConfigurableEnvironment) environment;
-	    }
-	  }
+	public DBConfigInitializer(JdbcTemplate jdbcTemplate) {
+		this.jdbcTemplate = jdbcTemplate;
 	}
+
+	@Override
+	public void afterPropertiesSet() throws Exception {
+		if (environment != null && environment.acceptsProfiles(Profiles.of("ci"))) {
+			System.out.println("Skipping DBConfigInitializer in 'ci' profile");
+			return;
+		}
+
+		if (null != environment) {
+			Map<String, Object> systemConfigMap = new HashMap<>();
+			String sql = "SELECT param, value from config_values";
+			List<Map<String, Object>> maps = jdbcTemplate.queryForList(sql);
+			for (Map<String, Object> map : maps) {
+				String param = String.valueOf(map.get("param"));
+				Object value = map.get("value");
+				systemConfigMap.put(param, value);
+				System.out.println(String.format("param=%s, value=%s ", param, value));
+			}
+			environment.getPropertySources().addFirst(new MapPropertySource(propertySourceName, systemConfigMap));
+		}
+	}
+
+	@Override
+	public void setEnvironment(Environment environment) {
+		if (environment instanceof ConfigurableEnvironment) {
+			this.environment = (ConfigurableEnvironment) environment;
+		}
+	}
+}
